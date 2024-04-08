83036508b80600904e5e907ab26834ebc82411bd
Madeleine Norozkhani <madeleine.norozkhani@gmail.com>
2024-04-08 15:17:10 +0200

    init

---
+++ src/components/Landingpage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landingpage = () => {
+  return (
+    <div className="Landingpage_container">
+      <h1 className="Landingpage_header">ALMABA</h1>
+    </div>
+  );
+};
+
+export default Landingpage;
